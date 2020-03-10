46117ec15f32e5a575eba2f93bfa94d7d9f07140
Muhammad <muhammad.ali.72.27@gmail.com>
2020-03-09 23:10:07 -0700

    added event listeners for nav bar

--- app.js
+++ app.js
@@ -22,3 +22,25 @@ var controller = (function(ui){
         setupListeners();}
     }
 })(UImodule);
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', event=>{
+    if (!showMenu){
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        showMenu = true;
+    }else{
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        showMenu = false;
+    }
+});
