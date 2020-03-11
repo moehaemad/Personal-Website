eb8e0d16023a9d23b61fa81a7d8f954108abbad2
Muhammad <muhammad.ali.72.27@gmail.com>
2020-03-10 20:38:46 -0700

    added function and toggle classlist to add/remove classList easier

--- app.js
+++ app.js
@@ -25,22 +25,17 @@ var controller = (function(ui){
 
 
 const menuBtn = document.querySelector('.menu-btn');
+const menuItem = document.querySelector('.menu-nav__link');
 const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 
-let showMenu = false;
+function toggleMenu(){
+    hamburger.classList.toggle('open');
+    nav.classList.toggle('open');
+    menuNav.classList.toggle('open');
+}
 
-menuBtn.addEventListener('click', event=>{
-    if (!showMenu){
-        hamburger.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        showMenu = true;
-    }else{
-        hamburger.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        showMenu = false;
-    }
-});
+menuBtn.addEventListener('click', toggleMenu);
+
+// menuItem.addEventListener('click', toggleMenu);
