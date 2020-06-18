77ded39df6509e3582858c514619996c38629157
Muhammad <muhammad.ali.72.27@gmail.com>
2020-06-17 19:33:20 -0700

    restructured for webpack: moved image assets, changed index.html calls for css & js, removed temp.css, and changed routing for .get requests for moehaemad.ca/ to dist directory

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const sendMailRouter = require('./routes/sendMail');
 
 
-app.use(express.static(__dirname + '/src/'))
+app.use(express.static(__dirname + '/dist/'))
 console.log(__dirname)
 app.get('/', (req, res) =>{
     // res.send('Hello World to the Express JS app');
