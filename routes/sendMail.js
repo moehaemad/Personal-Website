528578d22b90c04d2f28fcc624ecd6d7517f17b0
Muhammad <muhammad.ali.72.27@gmail.com>
2020-06-17 18:22:22 -0700

    added dotenv package to hide email and password for nodemailer

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const sendMailRouter = express.Router();
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-// let transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port
-// })
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth:{
+        user: NODEMAILER_EMAIL,
+        pass: NODEMAILER_PASS
+    }
+});
 
 
 sendMailRouter.get('/',req => {
