034f1e3cffdaabfdf1d6949dcc8aae4c427e9c52
Muhammad <muhammad.ali.72.27@gmail.com>
2020-06-21 16:12:07 -0700

    directed LudoGame router to point to dist folder, deleted unused resources, renamed file to avoid issues referencing

--- routes/ludoGame.js
+++ routes/ludoGame.js
@@ -4,16 +4,18 @@ const path = require('path');
 
 ludoRouter.use('/resources/', express.static(path.join(__dirname, '../src/LudoGame/resources')));
 
-ludoRouter.use ('/css/', express.static(path.join(__dirname, '../src/LudoGame/css')));
+ludoRouter.use ('/css/', express.static(path.join(__dirname, '../dist/css')));
 
-ludoRouter.use ('/js/', express.static(path.join(__dirname, '../src/LudoGame/js')));
+ludoRouter.use ('/js/', express.static(path.join(__dirname, '../dist/js')));
+
+ludoRouter.use ('/images/', express.static(path.join(__dirname, '../dist/images')));
 
 ludoRouter.get('/', (req, res) => {
     // res.render(ludo, (err, html) =>{
     //     if (err) res.send(err);
     //     res.send(html);
     // });
-    res.sendFile(path.join(__dirname, '../src/LudoGame/') + 'ludo.html');
+    res.sendFile(path.join(__dirname, '../dist/') + 'ludo.html');
 });
 
 module.exports = ludoRouter;
