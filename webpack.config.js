cad9cc5414ad68129d549c1c10611cd2e3266afd
Muhammad <muhammad.ali.72.27@gmail.com>
2020-06-22 22:05:39 -0700

    added react dependencies and loaders for jsx/css

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,22 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/main_bundle.js'
     },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/env"]
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
     plugins: [
         new htmlWebpackPlugin ({
             // it's ../ because the path for this configuration is to the js directory in output.
