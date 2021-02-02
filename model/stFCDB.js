bd989bce554610beded875b8351645463b4d051c
Muhammad <muhammad.ali.72.27@gmail.com>
2021-02-01 21:14:02 -0800

    renamed deck table and rename 'back' to 'description' and created text cards table

--- model/stFCDB.js
+++ model/stFCDB.js
@@ -1,20 +1,20 @@
 const {Pool} = require('pg');
 const dbConfig = require('./dbconfig');
 
-// let pool = new Pool({
-//     user: dbConfig.awsUser,
-//     databse: dbConfig.localdb,
-//     password: dbConfig.awsPass,
-//     host: dbConfig.PGHOST,
-//     port: 5432
-// });
-// TODO: return to 
 let pool = new Pool({
-    user: dbConfig.localuser,
-    database: dbConfig.localdb,
-    host: 'localhost',
-    password: dbConfig.localpass
-})
+    user: dbConfig.awsUser,
+    databse: dbConfig.localdb,
+    password: dbConfig.awsPass,
+    host: dbConfig.PGHOST,
+    port: 5432
+});
+// TODO: return to 
+// let pool = new Pool({
+//     user: dbConfig.localuser,
+//     database: dbConfig.localdb,
+//     host: 'localhost',
+//     password: dbConfig.localpass
+// })
 
 // has stfc_user (username text primary key, password text);
 // has stfc_deck_text (id integer primary key, username text references stfc_user(username), front text, back text);
@@ -39,23 +39,40 @@ const checkUser = (req, res) => {
 
 const createUser = (req, res) => {
     try{
-        // params: username, pass
-        pool.query(`insert into stfc_user values ('${req.params.username}', '${req.params.pass}');`);
+        /*post body: {username: String, pass: String} */
+        pool.query(`insert into stfc_user values ('${req.body.username}', '${req.body.pass}');`);
+        console.log(req.body.username);
+        console.log(req.body);
 
     }catch(err){
         console.log('error in stFC creating user', err);
     }
 }
 
-const insertDeck = (req, res) => {
+const createDeck = (req, res) => {
     try{
-        // params: id, username, front, back
-        // require id, username, front, and back
-        pool.query(`insert into stfc_deck_text values (${req.params.id}, '${req.params.username}', '${req.params.front}', '${req.params.back}' );`);
+        /*post body: {id: Number, username: String,
+            description: String} */
+
+        pool.query(`insert into stfc_deck values (${req.body.id}, '${req.body.username}', '${req.body.description}' );`);
 
     }catch(err){
         console.log('error in stFC creating user', err);
     }
 }
 
-module.exports = {createUser, insertDeck, checkUser};
+
+
+// TODO: finish query
+const getFlashCards = (req, res) => {
+    /* given a deck id get list of id's for the deck
+        params: 
+            id: Integer */
+    try{
+        res.status(200)
+    }catch(err){
+        res.status(500);
+    }
+}
+
+module.exports = {checkUser, createUser, createDeck};
